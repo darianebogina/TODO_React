502edb1a93f825e022ec31b6fd7ee7bbd2742c5c
Daria <dasha.nebogina@mail.ru>
2025-08-10 10:52:13 +0300

    feat: implement buttons actions

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,49 +1,65 @@
-// import {useState} from 'react'
+import {useState} from 'react'
 import './App.css'
 
+type Task = {
+    id: string;
+    text: string;
+}
+
 export function App() {
-    // const [count, setCount] = useState(0)
+    const [tasks, setTasks] = useState<Array<Task>>([]);
+
+    const addTask = (newText: string) => {
+        if (newText.trim() === "") {
+            alert("Please enter a new task");
+        }
+        const id: string = Math.floor(Math.random() * 1000).toString();
+        const newTask = {id, text: newText};
+        setTasks(prev => [newTask, ...prev]);
+    }
+
+    const deleteTask = (idToDelete: string) => {
+        setTasks(prev => prev.filter((task: Task) => task.id !== idToDelete));
+    }
 
     return (
         <>
-            <NewTaskForm/>
-            <TasksList tasks={["Three", "Four"]}/>
+            <NewTaskForm onAdd={addTask}/>
+            <TasksList taskList={tasks} deleteTask={deleteTask}/>
         </>
     )
 }
 
-const NewTaskForm = () => {
+const NewTaskForm = ({onAdd}: { onAdd: (newText: string) => void }) => {
+    const [newText, setNewText] = useState<string>("");
     return (
         <div>
             <h1>ToDo List</h1>
             <div>
-                <input type="text" placeholder="Add task"/>
-                <button>Add</button>
+                <input type="text" placeholder="Add task" onChange={(e) => setNewText(e.target.value)}/>
+                <button onClick={() => onAdd(newText)}>Add</button>
             </div>
         </div>
     );
 }
 
-const TasksList = ({tasks}: { tasks: Array<string> }) => {
+const TasksList = ({taskList, deleteTask}: { taskList: Array<Task>, deleteTask: (idToDelete: string) => void }) => {
     return (
         <div>
             <ul>
-                <li>
-                    <TaskRow task={tasks[0]}/>
-                </li>
-                <li>
-                    <TaskRow task={tasks[1]}/>
-                </li>
+                {taskList.map(task => (
+                    <TaskRow key={task.id} task={task} onDelete={deleteTask}/>
+                ))}
             </ul>
         </div>
     );
 }
 
-const TaskRow = ({task}: { task: string }) => {
+const TaskRow = ({task, onDelete}: { task: Task, onDelete: (idToDelete: string) => void }) => {
     return (
-        <>
-            <p>{task}</p>
-            <button>Delete</button>
-        </>
+        <li>
+            <p>{task.text}</p>
+            <button onClick={() => onDelete(task.id)}>&#10060;</button>
+        </li>
     );
 }
