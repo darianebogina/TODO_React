ea3f84dc73ae254a5ad1d9962fcf57956ba42bb0
Daria <dasha.nebogina@mail.ru>
2025-08-11 16:08:53 +0300

    feat: implement useEffect and useLayoutEffect

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect, useLayoutEffect,useRef} from 'react'
 import './App.css'
 
 type Task = {
@@ -8,6 +8,8 @@ type Task = {
 
 export function App() {
     const [tasks, setTasks] = useState<Array<Task>>([]);
+    const [loading, setLoading] = useState(true);
+    const deleteUsed = useRef(false);
 
     const addTask = (newText: string) => {
         if (newText.trim() === "") {
@@ -20,25 +22,76 @@ export function App() {
     }
 
     const deleteTask = (idToDelete: string) => {
+        deleteUsed.current = true;
         setTasks(prev => prev.filter((task: Task) => task.id !== idToDelete));
     }
 
+    // Week 3: day 1
+    useEffect(() => {
+        // console.log("useEffect on tasks change")
+        if ((deleteUsed.current && tasks.length === 0) || tasks.length !== 0) {
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+        }
+    }, [tasks]);
+
+    useEffect(() => {
+        console.log("useEffect (Parent)")
+        const tasksString = localStorage.getItem('tasks');
+        if (tasksString) {
+            setTasks(JSON.parse(tasksString));
+        }
+
+        return () => {
+            console.log("useEffect (Clean-Up Parent)")
+        }
+    }, []);
+
+    useLayoutEffect(() => {
+        console.log("useLayoutEffect (Parent)");
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 1500)
+
+        return () => {
+            console.log("useLayoutEffect (Clean-Up Parent)");
+            clearTimeout(timer);}
+    }, [])
+
+    //
     return (
         <>
             <NewTaskForm onAdd={addTask}/>
-            <TasksList taskList={tasks} deleteTask={deleteTask}/>
+            {loading ? <p> Loading...</p> : <TasksList taskList={tasks} deleteTask={deleteTask} />}
         </>
     )
 }
 
 const NewTaskForm = ({onAdd}: { onAdd: (newText: string) => void }) => {
     const [newText, setNewText] = useState<string>("");
+    useEffect(() => {
+        console.log("useEffect (Child)");
+        return () => {
+            console.log("useEffect (Clean-Up Child)");
+        }
+    }, []);
+
+    useLayoutEffect(() => {
+        console.log("useLayoutEffect (Child)");
+        return () => {
+            console.log("useLayoutEffect (Clean-Up Child)");
+        }
+    }, []);
+
     return (
         <div>
             <h1>ToDo List</h1>
             <div>
                 <input type="text" placeholder="Add task" value={newText} onChange={(e) => setNewText(e.target.value)}/>
-                <button className="add-btn" onClick={() => {onAdd(newText); setNewText("");}}>Add</button>
+                <button className="add-btn" onClick={() => {
+                    onAdd(newText);
+                    setNewText("");
+                }}>Add
+                </button>
             </div>
         </div>
     );
